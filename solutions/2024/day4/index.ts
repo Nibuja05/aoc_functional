c6a87607caced8df02d23f44d237b7b85408e8f4
Nibuja <antonwinkler98@web.de>
2024-12-04 18:18:30 +0100

    day4 format

--- solutions/2024/day4/index.ts
+++ solutions/2024/day4/index.ts
@@ -6,8 +6,9 @@ console.time("Part 1")
 getInput()
     .split("\n")
     .map(line => line.split(""))
-    .use(tabs => tabs.map((row, i) => 
-            row.map((item, j) => item != "X" && item != "S"
+    .use(tabs => tabs
+        .map((row, i) => row
+            .map((item, j) => item != "X" && item != "S"
                 ? [""]
                 : [[1,0],[2,0],[3,0],[0,1],[0,2],[0,3],[1,1],[2,2],[3,3],[1,-1],[2,-2],[3,-3]]
                     .reduce((acc, val, index) => (acc.at(-1) + (tabs[i + val[0]]?.[j + val[1]] ?? "")).use(cur => (index + 1) % 3 == 0
@@ -17,7 +18,7 @@ getInput()
                             : [...acc.slice(0, -1), cur]
                         )
                         ,[item]
-                )
+                    )
             )
         )
     )
@@ -37,18 +38,20 @@ console.time("Part 2")
 getInput2()
     .split("\n")
     .map(line => line.split(""))
-    .use(tabs => tabs.map((row, i) => 
-        row.map((item, j) => item != "A"
-            ? 0
-            : [[-1,1],[1,1],[1,-1],[-1,-1]]
-                .map(([x, y]) => tabs[i + x]?.[ j + y] ?? "")
-                .use(arr => arr[0] == arr[2]
-                    ? 0
-                    : arr.filter(c => c == "M").length == 2 && arr.filter(c => c == "S").length == 2
-                        ? 1
-                        : 0
-                )
-        ))
+    .use(tabs => tabs
+        .map((row, i) => row
+            .map((item, j) => item != "A"
+                ? 0
+                : [[-1,1],[1,1],[1,-1],[-1,-1]]
+                    .map(([x, y]) => tabs[i + x]?.[ j + y] ?? "")
+                    .use(arr => arr[0] == arr[2]
+                        ? 0
+                        : arr.filter(c => c == "M").length == 2 && arr.filter(c => c == "S").length == 2
+                            ? 1
+                            : 0
+                    )
+            )
+        )
     )
     .map(rows => rows
         .reduce((acc, val) => acc + val, <number>0)
